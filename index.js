4a96965b64b00ebd59bab14bf326b5facca71524
Cristian Contreras <contreras.cristian994@gmail.com>
2022-10-08 11:07:57 -0400

    commit tree 24897b5b51e72cf67064596f48a1278063447bab

---
+++ index.js
@@ -0,0 +1,11 @@
+function introduction(name = "Cris") {
+    return(`Hi, my name is ${name}.`);
+}
+
+function introductionWithLanguage(name = "Cris", language = "JavaScript") {
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+
+function introductionWithLanguageOptional(name = "Cris", language = "JavaScript") {
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
